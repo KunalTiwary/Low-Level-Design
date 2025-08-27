7fb57c439c313686bb7d5ffe91dd211d07d63d5d
Kunal Tiwary <76396930+KunalTiwary@users.noreply.github.com>
2025-08-27 15:08:18 +0530

    Update RemoteControl.py

--- Design Pattern/Command Design Pattern/RemoteControl.py
+++ Design Pattern/Command Design Pattern/RemoteControl.py
@@ -4,14 +4,18 @@ from .Command import Command
 class RemoteControl:
     def __init__(self):
         self.command = None
+        self.stack = []
 
     def set_command(self, command: Command):
         self.command = command
 
     def press_button(self):
         if self.command:
+            self.stack.append(self.command)
             self.command.execute()
 
-    def press_undo(self):
-        if self.command:
-            self.command.undo()
+    def press_undo(self, times):
+        if self.stack and times:
+            lastCommand = self.stack.pop()
+            lastCommand.undo()
+            times -= 1
